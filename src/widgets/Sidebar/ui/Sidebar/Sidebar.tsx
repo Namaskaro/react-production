69f3e5299cb0356d71a68217081d92bcc6619d0d
namaskaro <german.saratov@gmail.com>
2023-06-05 00:23:29 +0400

    fix toggle button

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,11 +3,13 @@ import cls from './Sidebar.module.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LanguageSwitcher } from 'widgets/LanguageSwitcher';
+import { useTranslation } from 'react-i18next';
 interface SidebarProps {
   className?: string;
 }
 
 export const Sidebar: React.FC<SidebarProps> = ({ className }: SidebarProps) => {
+  const { t } = useTranslation();
   const [collapsed, setCollapsed] = React.useState(false);
   const onToggle = () => {
     setCollapsed((prev) => !prev);
@@ -17,7 +19,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ className }: SidebarProps) => 
       data-testid="sidebar"
       className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
       <button data-testid="sidebar-toggle" onClick={onToggle}>
-        toggle
+        {t('Переключить')}
       </button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
